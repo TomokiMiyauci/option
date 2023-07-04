9d77de2524670291d9f36b87bc5d7d56d59a39f6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-04 22:18:35 +0900

    feat(logical): add logical and operator for option

--- operators/logical.ts
+++ operators/logical.ts
@@ -1,7 +1,7 @@
 // Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
-import { isSome } from "./query.ts";
+import { isNone, isSome } from "./query.ts";
 import { type Option } from "../spec.ts";
 
 /** Returns the {@link option} if it contains a value, otherwise returns {@link obtb}.
@@ -22,4 +22,27 @@ export function or<T>(option: Option<T>, obtb: Option<T>): Option<T> {
   if (isSome(option)) return option;
 
   return obtb;
+}
+
+/** Returns `None` if the `option` is `None`, otherwise returns `optb`.
+ *
+ * @example
+ * ```ts
+ * import { and } from "https://deno.land/x/optio/operators/logical.ts";
+ * import { None, Some } from "https://deno.land/x/optio/spec.ts";
+ * import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+ *
+ * declare const some: Some<unknown>;
+ * declare const someb: Some<unknown>;
+ *
+ * assertEquals(and(some, None), None);
+ * assertEquals(and(some, someb), someb);
+ * assertEquals(and(None, some), None);
+ * assertEquals(and(None, None), None);
+ * ```
+ */
+export function and<T>(option: Option<unknown>, optb: Option<T>): Option<T> {
+  if (isNone(option)) return option;
+
+  return optb;
 }
--- operators/logical_test.ts
+++ operators/logical_test.ts
@@ -1,7 +1,7 @@
 // Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
-import { or } from "./logical.ts";
+import { and, or } from "./logical.ts";
 import { None, Some } from "../spec.ts";
 import { assert, describe, it } from "../_dev_deps.ts";
 
@@ -16,5 +16,17 @@ describe("or", () => {
     const optb = Some.of(0);
     assert(or(None, optb) === optb);
     assert(or(None, None) === None);
+  });
+});
+
+describe("and", () => {
+  it("should return Some if both of arg is some otherwise None", () => {
+    const some = Some.of(0);
+    const someb = Some.of(1);
+
+    assert(and(some, None) === None);
+    assert(and(some, someb) === someb);
+    assert(and(None, some) === None);
+    assert(and(None, None) === None);
   });
 });
