4fee41ce65b5112a9907158749fe0ed370358304
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-04 17:24:38 +0900

    feat(query): add checking option type functions

---
+++ operators/query.ts
@@ -0,0 +1,34 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+import { type None, type Option, OptionType, type Some } from "../spec.ts";
+
+/** Returns `true` if the {@link option} is a {@link Some}.
+ *
+ * @example
+ * ```ts
+ * import { isSome, Option, Some } from "https://deno.land/x/optio/mod.ts";
+ * import { assert } from "https://deno.land/std/testing/asserts.ts";
+ *
+ * const option: Option<number> = Some.of(2);
+ * assert(isSome(option));
+ * ```
+ */
+export function isSome<T>(option: Readonly<Option<T>>): option is Some<T> {
+  return option.type === OptionType.Some;
+}
+
+/** Returns `true` if the {@link option} is a {@link None}.
+ *
+ * @example
+ * ```ts
+ * import { isNone, None, Option } from "https://deno.land/x/optio/mod.ts";
+ * import { assert } from "https://deno.land/std/testing/asserts.ts";
+ *
+ * const option: Option<unknown> = None;
+ * assert(isNone(option));
+ * ```
+ */
+export function isNone(option: Readonly<Option<unknown>>): option is None {
+  return option.type === OptionType.None;
+}
---
+++ operators/query_test.ts
@@ -0,0 +1,25 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+
+import { isNone, isSome } from "./query.ts";
+import { None, Some } from "../spec.ts";
+import { assert, assertFalse, describe, it } from "../_dev_deps.ts";
+
+describe("isSome", () => {
+  it("should return true if it is Some", () => {
+    assert(isSome(Some.of(0)));
+  });
+
+  it("should return false if it is None", () => {
+    assertFalse(isSome(None));
+  });
+});
+
+describe("isNone", () => {
+  it("should return true if it is None", () => {
+    assert(isNone(None));
+  });
+
+  it("should return false if it is Some", () => {
+    assertFalse(isNone(Some.of(0)));
+  });
+});
